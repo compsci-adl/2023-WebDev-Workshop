5d875b29c2b0548658e265f35a29439fa9fffaba
P0u4a <pouyamiria@gmail.com>
2023-08-25 14:34:39 +0930

    Move project and socials content to seperate files

---
+++ react-ts-tailwind/content/projects.ts
@@ -0,0 +1,32 @@
+export const PROJECTS = [
+    {
+        title: 'Constitution',
+        description:
+            'Constitution for the Computer Science Club of the University of Adelaide, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    },
+    {
+        title: 'Duck CTF',
+        description:
+            'Challenge repository for DuckCTF 2022 and 2023, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    },
+    {
+        title: 'Website',
+        description:
+            "The Computer Science Club's website repository, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    },
+    {
+        title: 'Registration App',
+        description:
+            'An app to collect CS Club user registrations, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    },
+    {
+        title: 'TicTacToe',
+        description:
+            'Game of Tic Tac Toe for club recruiting events, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    },
+    {
+        title: 'Utils',
+        description:
+            'Miscellaneous utilities used by the CS Club, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    },
+];
