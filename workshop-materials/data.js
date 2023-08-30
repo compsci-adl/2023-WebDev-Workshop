31620721e248612f10a70e2abcb32af78c4e3d77
Milan <milan.338@outlook.com>
2023-08-30 14:09:51 +0930

    Add workshop materials

---
+++ workshop-materials/data.js
@@ -0,0 +1,54 @@
+const socials = [
+    { title: 'Email me', link: 'mailto:contact@csclub.org.au' },
+    { title: 'Linkedin', link: 'https://linkedin.com/company/compsci-adl' },
+    { title: 'GitHub', link: 'https://github.com/compsci-adl' },
+];
+
+const projects = [
+    {
+        title: 'Constitution',
+        description:
+            'Constitution for the Computer Science Club of the University of Adelaide, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        link: 'https://github.com/compsci-adl/Constitution',
+    },
+    {
+        title: 'Duck CTF',
+        description:
+            'Challenge repository for DuckCTF 2022 and 2023, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        link: 'https://github.com/compsci-adl/DuckCTF',
+    },
+    {
+        title: 'Website',
+        description:
+            "The Computer Science Club's website repository, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+        link: 'https://github.com/compsci-adl/website',
+    },
+    {
+        title: 'Registration App',
+        description:
+            'An app to collect CS Club user registrations, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        link: 'https://github.com/compsci-adl/CsClub-Registration-App',
+    },
+    {
+        title: 'TicTacToe',
+        description:
+            'Game of Tic Tac Toe for club recruiting events, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        link: 'https://github.com/compsci-adl/CsClub-TicTacToe',
+    },
+    {
+        title: 'Utils',
+        description:
+            'Miscellaneous utilities used by the CS Club, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+        link: 'https://github.com/compsci-adl/utils',
+    },
+];
+
+const sampleTexts = [
+    `I'm a computer science student at Adelaide University and a freelance software
+    engineer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore.`,
+    `I get busy in my spare time working on personal projects. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+    dolore eu fugiat nulla pariatur. You can see some of my favourites below.`,
+];
